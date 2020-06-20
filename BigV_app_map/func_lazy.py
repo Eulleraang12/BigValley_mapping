51abe57ebf2b7b40a20248cfeed159232f565e93
Euller Barros <euller.eng@gmail.com>
2020-06-20 20:54:48 -0300

    readme com guia para instalacao django

---
+++ BigV_app_map/func_lazy.py
@@ -0,0 +1,46 @@
+def seg_area_lazy():
+    setores = ['Aeronáutico/Aeroespacial',
+            "Agronégocio","Arquitetura e Designer",
+        " Arte e Cultura","Automotivo","Beleza",
+        'Biotecnologia',"Comida e Bebida",'Construção Civil',
+        "Educação","Farmacêutico","Gestão de Resíduos",
+        "Materias primas e químicos",
+        "Moda e Têxtil",'Mídia','Saúde',"Serviço",
+        'Tech: TI e Software',"Tech: Hardware e Equipamentos",
+        'Tech: Mobile','Turismo','Telecomunicações',
+        'Varejo','Naútico','Naval','Empresas/Negócios (Business to Business)',
+        'Consumidores (Business to Consumer)','Governo',
+            ]
+    y = []
+    for setor in setores:
+        x = ['{}'.format(setor[0:3]), '{}'.format(setor)]
+        y.append(x)
+    return y
+
+def seg_atu_lazy():
+    setores = ['Aeronáutico', "Agronégocio", 'Arquitetura e Design', 'Automotivo',
+                'Beleza','Biotecnologia','Comida e Bebida','Construção Civil','Educação',
+                'Farmacêutico','Gestão de Resíduos','Matérias Primas e Químicos','Moda e Textil',
+                'Midia','Saúde e Bem-estar','Serviço','Tech: TI e Software',"Tech: Hardware e Equipamentos",
+                'Tech: Mobile','Telecomunicações','Varejo','Naútico','Naval','Arte e Cultura','Turismo',
+            ]
+    y = []
+    for setor in setores:
+        x = ['{}'.format(setor[0:3]), '{}'.format(setor)]
+        y.append(x)
+    return y
+
+def receita_lazy():
+
+    setores = [ 'Venda Unitária (Produto)','Venda Direta','Venda Indireta (agentes ou representantes)',
+                'Freemium','Software as a service (SaaS)','Receita com anúncios','Licença por usuário',
+                'Licença por produto','Sucessfee','Homem hora','Comissão','Markup','Other',
+            ]
+    y = []
+    for setor in setores:
+        x = ['{}'.format(setor[0:3]), '{}'.format(setor)]
+        y.append(x)
+    return y
+
+
+
---
+++ Readme.txt
@@ -0,0 +1,15 @@
+passo a passo para rodar em uma maquina 
+
+1- instalar o python 3
+https://www.python.org/downloads/
+
+2- criar um ambiente virtual 
+pip install virtualenv
+
+mkvirtual [nomedoambiente]
+
+pip install virtualenvwrapper-win (para versionar o ambiente com workon)
+
+3- pip install -r requirements.txt
+
+4- python manage.py runserver (para executar o "serve")
